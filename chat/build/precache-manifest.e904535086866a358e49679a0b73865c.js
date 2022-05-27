04cbc6457bf1a975e1dbdd8668ccd6d6466db2e0
aloony <npp3rzwi@gmail.com>
2022-05-27 20:48:33 +0200

    initial commit

---
+++ chat/build/precache-manifest.e904535086866a358e49679a0b73865c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "378dfa4228fe750b4804",
+    "url": "/static/css/main.d6245f13.chunk.css"
+  },
+  {
+    "revision": "378dfa4228fe750b4804",
+    "url": "/static/js/main.378dfa42.chunk.js"
+  },
+  {
+    "revision": "b1cc2dc065e0611b48d6",
+    "url": "/static/css/1.bebdda16.chunk.css"
+  },
+  {
+    "revision": "b1cc2dc065e0611b48d6",
+    "url": "/static/js/1.b1cc2dc0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8f45987d8a81779dd2029cd9e1fec8e",
+    "url": "/index.html"
+  }
+];
---
+++ chat/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rootkid/ногайне покажи пенис/django-portfolio/chat/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
